0cdfd40435ad1e75363c7744d719205698eb0899
Harry Heintz <harryheintz@me.com>
2012-01-02 12:48:13 -0500

    rertry

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Random Task"
+  user.email                  "example@what.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
